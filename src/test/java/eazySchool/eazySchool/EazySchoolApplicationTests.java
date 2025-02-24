f7c2cf41853ccc850baf88722e67ab1ec2bd2f30
xinghmp968 <rahulsinghmp968@gmail.com>
2025-02-24 22:22:08 +0000

    revision eazybyte school app

---
+++ src/test/java/eazySchool/eazySchool/EazySchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package eazySchool.eazySchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazySchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
